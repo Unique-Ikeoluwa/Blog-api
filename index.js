a66a29e2233c21923bfa09a3c225ea2ac4d2a40e
Unique-Ikeoluwa <ikeoluwaadegbite@gmail.com>
2025-10-24 17:10:05 +0100

    Blog api

---
+++ index.js
@@ -0,0 +1,200 @@
+const http = require("http")
+const fs = require("fs").promises
+
+const PORT = 3000
+
+async function readUsers() {
+    const data = await fs.readFile("users.json", "utf8")
+    return JSON.parse(data)
+}
+
+async function readPosts() {
+    const data = await fs.readFile("posts.json", "utf8")
+    return JSON.parse(data)
+}
+
+async function writeUsers(data) {
+    await fs.writeFile("users.json", JSON.stringify(data, null, 2))
+}
+
+async function writePosts(data) {
+    await fs.writeFile("posts.json", JSON.stringify(data, null, 2))
+}
+
+
+const server = http.createServer(async (req, res) => {
+    res.setHeader("Content-Type", "application/json")
+    const url = req.url
+    const method = req.method
+    try {
+        if (url === "/posts" && method === "GET"){
+            const posts = await readPosts()
+            res.writeHead(200)
+            return res.end(JSON.stringify({success: true, data: posts}))
+        }
+
+        else if(url === "/register" && method === "POST"){
+            const users = await readUsers()
+            const newId = users.length ? Math.max(...users.map(user => user.id)) + 1 : 1
+            let body = ""
+            req.on("data", chunk => (body += chunk))
+            req.on("end", async () => {
+                const {email, name, password } = JSON.parse(body)
+                if (!email || !name || !password){
+                    res.writeHead(400)
+                    return res.end(JSON.stringify({success: false, message: "All fields are required"}))
+                }
+                const existingUser = users.find(user => user.email === email)
+                if (existingUser){
+                    res.writeHead(409)
+                    return res.end(JSON.stringify({success: false, message: "User already exist"}))
+                }
+                const newUser = {
+                    id: newId,
+                    email,
+                    name,
+                    password
+                }
+                users.push(newUser)
+                await writeUsers(users)
+                res.writeHead(201)
+                return res.end(JSON.stringify({success: true, message: "User registered successfully", data: newUser}))
+            })
+            return
+        }
+
+        else if (url === "/login" && method === "POST"){
+            const users = await readUsers()
+            let body = ""
+            req.on("data", chunk => (body += chunk))
+            req.on("end", async () => {
+                const {email, password } = JSON.parse(body)
+                if(!email || !password){
+                    res.writeHead(400)
+                    return res.end(JSON.stringify({success: false, message: "All fields required"}))
+                }
+                const validUser = users.find(user => user.email === email)
+                if (!validUser){
+                    res.writeHead(401)
+                    return res.end(JSON.stringify({success: false, message: "Invalid credentials"}))
+                }
+                if (validUser.password !== password){
+                    res.writeHead(401)
+                    return res.end(JSON.stringify({success: false, message: "Incorrect password"}))
+                }
+                res.writeHead(200)
+                return res.end(JSON.stringify({success: true, message: "Login successful", data: validUser}))
+            })
+            return
+        }
+
+        else if (url.startsWith("/posts/") && method === "GET"){
+            const posts = await readPosts()
+            const id = parseInt(url.split("/")[2])
+            const post = posts.find(p => p.id === id)
+            if (!post){
+                res.writeHead(404)
+                return res.end(JSON.stringify({success: false, message: "No post found"}))
+            }
+            res.writeHead(200)
+            return res.end(JSON.stringify({success: true, data: post}))
+        }
+
+        else if (url === "/createpost" && method === "POST"){
+            const posts = await readPosts()
+            let body = ""
+            req.on("data", chunk => (body += chunk))
+            req.on("end", async () => {
+                const { title, details, author } = JSON.parse(body)
+                if (!title || !details || !author ){
+                    res.writeHead(400)
+                    return res.end(JSON.stringify({success: false, message: "All fields required"}))
+                }
+                const newPostId = posts.length ? Math.max(...posts.map(post => post.id)) + 1 : 1
+                const newPost = {
+                    id: newPostId,
+                    title,
+                    details,
+                    author
+                }
+                posts.push(newPost)
+                await writePosts(posts)
+                res.writeHead(201)
+                return res.end(JSON.stringify({success: true, data: newPost, message: "Post added successfully"}))
+            })
+            return
+        }
+        
+        else if (url.startsWith("/posts/") && method === "PUT"){
+            const posts = await readPosts()
+            const id = parseInt(url.split("/")[2])
+            let body = ""
+            req.on("data", chunk => (body += chunk))
+            req.on("end", async () => {
+                const { title, details, author } = JSON.parse(body)
+                if (!title || !details || !author ){
+                    res.writeHead(400)
+                    return res.end(JSON.stringify({success: false, message: "All fields required"}))
+                }
+                const index = posts.findIndex(post => post.id === id)
+                if (index === -1 ){
+                    res.writeHead(404)
+                    return res.end(JSON.stringify({success: false, message: "Post not found"}))
+                }
+                posts[index] = {...posts[index], title, details, author}
+                await writePosts(posts)
+                res.writeHead(200)
+                return res.end(JSON.stringify({success: true, data: posts[index], message: "Post updated successfully"}))
+            })
+            return
+        }
+
+        else if (url.startsWith("/posts/") && method === "PATCH"){
+            const posts = await readPosts()
+            const id = parseInt(url.split("/")[2])
+            let body = ""
+            req.on("data", chunk => (body += chunk))
+            req.on("end", async () => {
+                const updates = JSON.parse(body)
+                if (Object.keys(updates).length === 0) {
+                    res.writeHead(400)
+                    return res.end(JSON.stringify({ success: false, message: "No fields to update" }))
+                    }
+                const index = posts.findIndex(post => post.id === id)
+                if (index === -1 ){
+                    res.writeHead(404)
+                    return res.end(JSON.stringify({success: false, message: "Post not found"}))
+                }
+                posts[index] = {...posts[index], ...updates}
+                await writePosts(posts)
+                res.writeHead(200)
+                return res.end(JSON.stringify({success: true, data: posts[index], message: "Post updated successfully"}))
+            })
+            return
+        }
+
+        else if (url.startsWith("/posts/") && method === "DELETE"){
+            const posts = await readPosts()
+            const id = parseInt(url.split("/")[2])
+            const newPosts = posts.filter(post => post.id !== id)
+            if (newPosts.length === posts.length){
+                res.writeHead(404)
+                return res.end(JSON.stringify({success: false, message: "Post not found"}))
+            }
+            await writePosts(newPosts)
+            res.writeHead(200)
+            return res.end(JSON.stringify({success: true, message: "Post deleted successfully"}))
+        }
+
+        else {
+            res.writeHead(404)
+            return res.end(JSON.stringify({success: false, message: "Route not found"}))
+        }
+    } catch (error){
+        res.writeHead(500)
+        res.end(JSON.stringify({success: false, message: "Server error", error: error.message}))
+    }
+})
+server.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`)
+})
